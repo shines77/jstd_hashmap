ef00c78a332ba40082c3180e4683de73e21ce167
GuoXH <gz_shines@msn.com>
2022-07-16 11:32:22 +0800

    改用 jtest::GetCurrentMemoryUsage()
    Signed-off-by: GuoXH <gz_shines@msn.com>

--- bench/time_hash_map/time_hash_map.cpp
+++ bench/time_hash_map/time_hash_map.cpp
@@ -255,7 +255,7 @@ void reset_counter()
 static inline
 std::size_t CurrentMemoryUsage()
 {
-    return jtest::getReadRSS();
+    return jtest::GetCurrentMemoryUsage();
 }
 
 namespace test {
@@ -890,14 +890,18 @@ static void report_result(char const * title, double ut, std::size_t iters,
     // Construct heap growth report text if applicable
     char heap[128];
     heap[0] = '\0';
-#if 1
+#if 0
     if (end_memory > start_memory) {
         snprintf(heap, sizeof(heap), "%7.1f MB", (double)(end_memory - start_memory) / 1048576.0);
     }
 #else
-    snprintf(heap, sizeof(heap), "%7.1f MB - %7.1f MB",
-             (double)start_memory / 1048576.0,
-             (double)end_memory / 1048576.0);
+    if (start_memory != end_memory) {
+        snprintf(heap, sizeof(heap), "%7.1f MB - %7.1f MB",
+                 (double)start_memory / 1048576.0,
+                 (double)end_memory / 1048576.0);
+    } else if (start_memory != 0) {
+        snprintf(heap, sizeof(heap), "%7.1f MB", (double)start_memory / 1048576.0);
+    }
 #endif
 
 #if (USE_STAT_COUNTER == 0)
