82755f3db06b2f685beaa7b70a0694db91fdf684
GuoXH <gz_shines@msn.com>
2022-07-16 11:44:03 +0800

    修正正负号显示
    Signed-off-by: GuoXH <gz_shines@msn.com>

--- bench/time_hash_map/time_hash_map.cpp
+++ bench/time_hash_map/time_hash_map.cpp
@@ -895,11 +895,11 @@ static void report_result(char const * title, double ut, std::size_t iters,
         snprintf(heap, sizeof(heap), "%7.1f MB", (double)(end_memory - start_memory) / 1048576.0);
     }
 #else
-    if (start_memory > end_memory) {
+    if (end_memory > start_memory) {
         snprintf(heap, sizeof(heap), "%7.1f MB  (+ %0.1f MB)",
                  (double)start_memory / 1048576.0,
                  (double)(end_memory - start_memory) / 1048576.0);
-    } else if (start_memory < end_memory) {
+    } else if (start_memory > end_memory) {
         snprintf(heap, sizeof(heap), "%7.1f MB  (- %0.1f MB)",
                  (double)start_memory / 1048576.0,
                  (double)(start_memory - end_memory) / 1048576.0);
