5b94962ef685d5c2de548918b8232c5be64198a3
GuoXH <gz_shines@msn.com>
2022-07-24 00:40:27 +0800

    添加 ./cmake-clang.sh, ./clang/cmake-clang.sh
    Signed-off-by: GuoXH <gz_shines@msn.com>

---
+++ clang/cmake-clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make clean
+rm -rf ./CMakeFiles
+rm -rf ./bench/CMakeFiles
+rm -rf ./test/CMakeFiles
+rm ./bench/Makefile
+rm ./test/Makefile
+rm ./CMakeCache.txt
+rm ./cmake_install.cmake
