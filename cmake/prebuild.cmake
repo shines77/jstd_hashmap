cf1ab3a3c24bbb2f3310eb7da08715f7b148b931
shines77 <gz_shines@msn.com>
2024-12-28 19:16:23 +0800

    修正路径
    Signed-off-by: shines77 <gz_shines@msn.com>

--- cmake/prebuild.cmake
+++ cmake/prebuild.cmake
@@ -44,8 +44,8 @@ if (DEFINED CPUIDEMU)
 endif()
 
 set(TARGET_CONF "config_hw_detect.h")
-set(TARGET_CONF_DIR ${PROJECT_BINARY_DIR}/src/jstd/config/)
-set(TARGET_CONF_TARGET_DIR ${PROJECT_SOURCE_DIR}/src/jstd/config/)
+set(TARGET_CONF_DIR ${PROJECT_BINARY_DIR}/src/jstd/config)
+set(TARGET_CONF_TARGET_DIR ${PROJECT_SOURCE_DIR}/src/jstd/config)
 
 set(TARGET_CONF_TEMP "${PROJECT_BINARY_DIR}/src/jstd/config/${TARGET_CONF}.tmp")
 
