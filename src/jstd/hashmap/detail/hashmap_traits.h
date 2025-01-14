c122603ac0c2ccd19262151cec9823748cc69f30
shines77 <gz_shines@msn.com>
2025-01-14 13:11:22 +0800

    添加了 detail::hash_is_avalanching<Hash> traits;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/jstd/hashmap/detail/hashmap_traits.h
+++ src/jstd/hashmap/detail/hashmap_traits.h
@@ -22,6 +22,51 @@ using is_emplace_kv_able = std::integral_constant<bool,
     (jstd::is_similar<K, typename Container::key_type>::value ||
      jstd::is_complete_and_move_constructible<typename Container::key_type>::value)>;
 
+namespace hash_detail {
+
+template <typename IsAvalanching>
+struct avalanching_value
+{
+    static constexpr bool value = IsAvalanching::value;
+};
+
+/* may be explicitly marked as DEPRECATED in the future */
+template <>
+struct avalanching_value<void>
+{
+    static constexpr bool value = true;
+};
+
+template <typename Hash, typename = void>
+struct hash_is_avalanching_impl : std::false_type{};
+
+template <typename Hash>
+struct hash_is_avalanching_impl<Hash, jstd::void_t<typename Hash::is_avalanching>>
+    : std::integral_constant<bool, avalanching_value<typename Hash::is_avalanching>::value>
+{};
+
+/* Hash::is_avalanching is not a type: compile error downstream */
+template <typename Hash>
+struct hash_is_avalanching_impl<Hash, typename std::enable_if<((void)Hash::is_avalanching, true)>::type>
+{};
+
+} // namespace hash_detail
+
+/*
+ * Each trait can be partially specialized by users for concrete hash functions
+ * when actual characterization differs from default.
+ */
+
+/* 
+ * hash_is_avalanching<Hash>::value is:
+ *   - false if Hash::is_avalanching is not present.
+ *   - Hash::is_avalanching::value if this is present and constexpr-convertible to a bool.
+ *   - true if Hash::is_avalanching is void (deprecated).
+ *   - ill-formed otherwise.
+ */
+template <typename Hash>
+struct hash_is_avalanching : hash_detail::hash_is_avalanching_impl<Hash>::type {};
+
 } // namespace detail
 } // namespace jstd
 
